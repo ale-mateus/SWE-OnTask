4c89d1b93501a2103fc4134976004e46a77c6ead
deagan-kilpatrick <d.kilpatrick@ufl.edu>
2024-10-17 15:18:04 -0400

    schedule backbone done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Home from './pages/TeacherCreate'
-import Home from './pages/StudentParentCreate'
-import Home from './pages/DeleteTeacher'
-import Home from './pages/DeleteStudentParent'
+import Schedule from './Schedule.jsx'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/teacher/create" element={<TeacherCreate />} />
-      <Route path="/student/create" element={<StudentParentCreate />} />
-      <Route path="/teacher/delete/:id" element={<DeleteTeacher />} />
-      <Route path="/student/delete/:id" element={<DeleteStudentParent />} />
-    </Routes>
+    <Schedule />
   )
 }
 
